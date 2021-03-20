0ecd34b419284c586de0bd7586043917f0c7d749
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-20 16:31:08 +0100

    Basic platformer player scene

---
+++ PlatformerPlayer.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+var vel := Vector2.ZERO
+
+onready var gravity :Vector2 = (ProjectSettings.get("physics/2d/default_gravity") * 
+	ProjectSettings.get("physics/2d/default_gravity_vector"))
+
+func _physics_process(delta):
+	vel += gravity * delta
+	
+	vel = move_and_slide(vel, Vector2.UP)
