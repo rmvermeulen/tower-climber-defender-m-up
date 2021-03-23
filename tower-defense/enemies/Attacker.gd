0f8af0dee7ccc38f193f56f0825d778f9e9f6255
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-23 23:02:38 +0100

    Rename method for clarity

--- tower-defense/enemies/Attacker.gd
+++ tower-defense/enemies/Attacker.gd
@@ -16,13 +16,13 @@ func _find_target():
 	var towers := get_tree().get_nodes_in_group("towers")
 	if towers.empty():
 		return
-	towers.sort_custom(self, "_sort_by_distance")
+	towers.sort_custom(self, "_sort_by_proximity")
 	target = towers[0]
 	path = game.nav.find_path(position, target.position)
 	prints(name, path)
 
 
-func _sort_by_distance(a, b):
+func _sort_by_proximity(a, b):
 	var da := position.distance_squared_to(a.position)
 	var db := position.distance_squared_to(b.position)
 	return da < db
