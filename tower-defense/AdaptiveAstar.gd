9155e778bb19f1f10e08e058e74d79b22ddcd8d4
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-23 17:02:14 +0100

    AdaptiveAstar: use world position, api to block/clear single points

--- tower-defense/AdaptiveAstar.gd
+++ tower-defense/AdaptiveAstar.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 enum CONNECT_MODE { STRAIGHT, STRAIGHT_AND_DIAGONAL }
 
-const GRID_CELL_SIZE := 64
+const GRID_CELL_SIZE := 64.0
 export (CONNECT_MODE) var connect_mode := CONNECT_MODE.STRAIGHT_AND_DIAGONAL
 
 onready var astar := AStar2D.new()
@@ -12,18 +12,19 @@ onready var rect := $CollisionShape2D.shape as RectangleShape2D
 func _ready():
 	var cell_count := rect.extents / GRID_CELL_SIZE
 	var id := 0
-	for y in range(-cell_count.y, cell_count.y):
-		for x in range(-cell_count.x, cell_count.x):
-			astar.add_point(id, Vector2(x, y), 1.0)
+	for cy in range(-cell_count.y, cell_count.y):
+		for cx in range(-cell_count.x, cell_count.x):
+			var pos := Vector2(cx, cy) * GRID_CELL_SIZE
+			astar.add_point(id, pos, 1.0)
 			id += 1
 	var connected := 0
 	var skipped := 0
 	var max_dist: float
 	match connect_mode:
 		CONNECT_MODE.STRAIGHT:
-			max_dist = 2
+			max_dist = pow(GRID_CELL_SIZE * 1.1, 2)
 		CONNECT_MODE.STRAIGHT_AND_DIAGONAL:
-			max_dist = 3
+			max_dist = pow(GRID_CELL_SIZE * 1.5, 2)
 		_:
 			assert(false, "Invalid connect_mode")
 	prints('max dist', max_dist)
@@ -33,7 +34,8 @@ func _ready():
 				continue
 			var pa := astar.get_point_position(a)
 			var pb := astar.get_point_position(b)
-			if pa.distance_squared_to(pb) < max_dist:
+			var d := pa.distance_squared_to(pb)
+			if d < max_dist:
 				connected += 1
 				astar.connect_points(a, b)
 			else:
@@ -42,28 +44,35 @@ func _ready():
 	assert(OK == connect("body_entered", self, "_on_body_entered"))
 	assert(OK == connect("body_exited", self, "_on_body_exited"))
 
-	for body in get_overlapping_bodies():
-		_block_body_position(body)
+	while is_inside_tree():
+		yield(get_tree().create_timer(5.0), "timeout")
+		update()
 
 
 func _on_body_entered(body: StaticBody2D) -> void:
 	if not body:
 		return
-	_block_body_position(body)
+	block_cell(body.position, 0)
 
 
 func _on_body_exited(body: StaticBody2D) -> void:
 	if not body:
 		return
-	_clear_body_position(body)
+	clear_cell(body.position)
 
 
-func _block_body_position(body: StaticBody2D) -> void:
-	pass
+func block_cell(pos: Vector2, type: int) -> void:
+	var pid := astar.get_closest_point(pos)
+	astar.set_point_disabled(pid)
 
 
-func _clear_body_position(body: StaticBody2D) -> void:
-	pass
+func clear_cell(pos: Vector2) -> void:
+	var pid := astar.get_closest_point(pos, true)
+	astar.set_point_disabled(pid, false)
+
+
+func find_path(start: Vector2, end: Vector2, _options := {}) -> PoolVector2Array:
+	return astar.get_simple_path(start, end)
 
 
 func _draw():
@@ -78,13 +87,13 @@ func _draw():
 			if cs.has(key):
 				continue
 			cs[key] = [
-				idp * GRID_CELL_SIZE,
-				astar.get_point_position(pc) * GRID_CELL_SIZE,
+				idp,
+				astar.get_point_position(pc),
 				colors[randi() % colors.size()],
 			]
 	for args in cs.values():
 		callv("draw_line", args)
 
 	for id in astar.get_points():
-		var pos := GRID_CELL_SIZE * astar.get_point_position(id)
+		var pos := astar.get_point_position(id)
 		draw_circle(pos, 4, Color.black)
--- tower-defense/TowerDefenseTest.gd
+++ tower-defense/TowerDefenseTest.gd
@@ -2,13 +2,15 @@ extends SubGame
 
 
 func _ready():
-	assert(OK == self.state.connect("block_created", self, "_on_block_created"))
-	assert(OK == self.state.connect("block_destroyed", self, "_on_block_destroyed"))
+	if not state:
+		return
+	assert(OK == state.connect("block_created", self, "_on_block_created"))
+	assert(OK == state.connect("block_destroyed", self, "_on_block_destroyed"))
 
 
 func _on_block_created(pos: Vector2, type: String):
-	pass
+	$AdaptiveAstar.block_cell(pos, type)
 
 
 func _on_block_destroyed(pos: Vector2):
-	pass
+	$AdaptiveAstar.clear_cell(pos)
