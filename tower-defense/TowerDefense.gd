7d8eb2902b2129ed71756e3049c15f4330a8def4
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-23 21:26:54 +0100

    TowerDefense: create a shape inside the tree when state changes

--- tower-defense/TowerDefense.gd
+++ tower-defense/TowerDefense.gd
@@ -9,8 +9,20 @@ func _ready():
 
 
 func _on_block_created(pos: Vector2, type: String):
-	$AdaptiveAstar.block_cell(pos, type)
+	# TODO: use type
+	var shape := RectangleShape2D.new()
+	shape.extents = Vector2(32, 32)
+	var shape_node := CollisionShape2D.new()
+	shape_node.shape = shape
+	var body := StaticBody2D.new()
+	body.add_child(shape_node)
+
+	add_child(body)
 
 
 func _on_block_destroyed(pos: Vector2):
-	$AdaptiveAstar.clear_cell(pos)
+	for child in get_children():
+		if child is StaticBody2D && child.position == pos:
+			remove_child(child)
+			child.queue_free()
+			break
